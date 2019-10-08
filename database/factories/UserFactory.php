35bc995c06452d477fb1f0eebcef028b0705eee9
daguilarm <damian.aguilarm@gmail.com>
2019-10-08 15:11:17 +0200

    updated to L6

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ $factory->defineAs(App\User::class, 'admin', function ($faker) {
         'email'             => 'damian.aguilarm@gmail.com',
         'email_verified_at' => now(),
         'password'          => bcrypt('nav666'),
-        'remember_token'    => str_random(10),
+        'remember_token'    => Str::random(10),
     ];
 });
 
@@ -28,7 +29,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password'          => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token'    => str_random(10),
+        'remember_token'    => Str::random(10),
         'created_at'        => $faker->dateTimeBetween($startDate = '-1 year', $endDate = 'now'),
     ];
 });
