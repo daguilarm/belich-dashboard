aa85e6cf1eea4cf622d1635ed4decb4e93018371
daguilarm <damian.aguilarm@gmail.com>
2019-10-24 08:51:25 +0200

    starting with tests

---
+++ tests/Unit/Helpers/BladeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BladeTest extends TestCase
+{
+    /** Test */
+    public function test_hide_container_for_sm()
+    {
+        $expected = 'hidden sm:hidden md:flex lg:flex xl:flex';
+        $actual = hideContainerForScreens(['sm']);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** Test */
+    public function test_hide_container_for_md()
+    {
+        $expected = 'hidden sm:flex md:hidden lg:flex xl:flex';
+        $actual = hideContainerForScreens(['md']);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** Test */
+    public function test_hide_container_for_lg()
+    {
+        $expected = 'hidden sm:flex md:flex lg:hidden xl:flex';
+        $actual = hideContainerForScreens(['lg']);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** Test */
+    public function test_hide_container_for_xl()
+    {
+        $expected = 'hidden sm:flex md:flex lg:flex xl:hidden';
+        $actual = hideContainerForScreens(['xl']);
+        $this->assertEquals($expected, $actual);
+    }
+}
