e78b73dcda60e2d494d7944091808b252103ee7e
daguilarm <damian.aguilarm@gmail.com>
2019-10-25 09:55:05 +0200

    Fixed factories errors

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,14 +13,24 @@ use Illuminate\Support\Str;
 | model instances for testing / seeding your application's database.
 |
 */
-$factory->defineAs(App\User::class, 'admin', function ($faker) {
+
+$factory->defineAs(App\User::class, 'admin',  function ($faker) {
     return [
         'name'              => 'admin',
         'email'             => 'admin@email.com',
         'email_verified_at' => now(),
         'password'          => bcrypt('admin'),
-        'remember_token'    => Str::random(10),
+        'remember_token'    => Str::random(10)
     ];
+});
+
+$factory->afterCreatingState(App\User::class, 'admin', function ($user, $faker) {
+    factory(\App\Billing::class)->create([
+        'user_id' => $user->id
+    ]);
+    factory(\App\Profile::class)->create([
+        'user_id' => $user->id
+    ]);
 });
 
 $factory->define(App\User::class, function (Faker $faker) {
@@ -32,4 +42,14 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token'    => Str::random(10),
         'created_at'        => $faker->dateTimeBetween($startDate = '-1 year', $endDate = 'now'),
     ];
+});
+
+$factory->afterCreating(App\User::class, function ($user, $faker) {
+    factory(\App\Billing::class)->create([
+        'user_id' => $user->id,
+        'created_at' => $faker->dateTimeBetween($startDate = 'last year', $endDate = 'now')
+    ]);
+    factory(\App\Profile::class)->create([
+        'user_id' => $user->id
+    ]);
 });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,33 +18,8 @@ class UsersTableSeeder extends Seeder
 
         //Create the admin
         $admin = factory(\App\User::class, 'admin')->create();
-        $adminBilling = factory(\App\Billing::class)->create([
-            'user_id' => $admin->id
-        ]);
-        $adminProfile = factory(\App\Profile::class)->create([
-            'user_id' => $admin->id
-        ]);
 
-        for($x = 1; $x <= 100; $x++) {
-            $user = factory(\App\User::class)->create();
-            $userBilling = factory(\App\Billing::class)->create([
-                'user_id' => $user->id,
-                'created_at' => Faker::create()->dateTimeBetween($startDate = 'last month', $endDate = 'now'),
-            ]);
-            $userProfile = factory(\App\Profile::class)->create([
-                'user_id' => $user->id
-            ]);
-        }
-
-        for($x = 1; $x <= 500; $x++) {
-            $user = factory(\App\User::class)->create();
-            $userBilling = factory(\App\Billing::class)->create([
-                'user_id' => $user->id,
-                'created_at' => Faker::create()->dateTimeBetween($startDate = 'last year', $endDate = 'now'),
-            ]);
-            $userProfile = factory(\App\Profile::class)->create([
-                'user_id' => $user->id
-            ]);
-        }
+        //Create 200 users
+        $users = factory(\App\User::class, 200)->create();
     }
 }
