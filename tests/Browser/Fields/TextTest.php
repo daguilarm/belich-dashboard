f1bf8edd7d6feaae4496d0ae8476334af133910e
daguilarm <damian.aguilarm@gmail.com>
2019-10-28 20:21:23 +0100

    Added visibility test

--- tests/Browser/Fields/TextTest.php
+++ tests/Browser/Fields/TextTest.php
@@ -22,6 +22,7 @@ class TextTest extends DuskTestCase
 
         $this->user = factory(User::class)->create();
         $this->test = factory(Test::class)->create();
+        $this->field = '_fieldtexts';
     }
 
     /**
@@ -36,59 +37,9 @@ class TextTest extends DuskTestCase
             // Index visibility
             $browser
                 ->loginAs($this->user)
-                ->visit('dashboard/_fieldtexts')
-                ->assertSee(parent::visibilityText('Except on forms', 'index'))
-                ->assertSee(parent::visibilityText('Hide when creating', 'index'))
-                ->assertSee(parent::visibilityText('Hide when editing', 'index'))
-                ->assertDontSee(parent::visibilityText('Hide from index', 'index'))
-                ->assertDontSee(parent::visibilityText('Hide-from index', 'index'))
-                ->assertSee(parent::visibilityText('Hide from show', 'index'))
-                ->assertDontSee(parent::visibilityText('Only on forms', 'index'))
-                ->assertSee(parent::visibilityText('Only on index', 'index'))
-                ->assertDontSee(parent::visibilityText('Only on show', 'index'))
-                ->assertSee(parent::visibilityText('Visible-on index', 'index'));
-
-            // SHOW visibility
-            $browser
-                ->visit('dashboard/_fieldtexts/' . $this->user->id)
-                ->assertSee(parent::visibilityText('Except on forms', 'show'))
-                ->assertSee(parent::visibilityText('Hide when creating', 'show'))
-                ->assertSee(parent::visibilityText('Hide when editing', 'show'))
-                ->assertSee(parent::visibilityText('Hide from index', 'show'))
-                ->assertSee(parent::visibilityText('Hide-From index', 'show'))
-                ->assertDontSee(parent::visibilityText('Hide from show', 'show'))
-                ->assertDontSee(parent::visibilityText('Only on forms', 'show'))
-                ->assertDontSee(parent::visibilityText('Only on index', 'show'))
-                ->assertSee(parent::visibilityText('Only on show', 'show'))
-                ->assertDontSee(parent::visibilityText('Visible-On index', 'show'));
-
-                // Edit visibility
-                $browser
-                    ->visit('dashboard/_fieldtexts/' . $this->user->id . '/edit')
-                    ->assertDontSee(parent::visibilityText('Except on forms', 'show'))
-                    ->assertSee(parent::visibilityText('Hide when creating', 'show'))
-                    ->assertDontSee(parent::visibilityText('Hide when editing', 'show'))
-                    ->assertSee(parent::visibilityText('Hide from index', 'show'))
-                    ->assertSee(parent::visibilityText('Hide-From index', 'show'))
-                    ->assertSee(parent::visibilityText('Hide from show', 'show'))
-                    ->assertSee(parent::visibilityText('Only on forms', 'show'))
-                    ->assertDontSee(parent::visibilityText('Only on index', 'show'))
-                    ->assertDontSee(parent::visibilityText('Only on show', 'show'))
-                    ->assertDontSee(parent::visibilityText('Visible-On index', 'show'));
-
-            // Create visibility
-            $browser
-                ->visit('dashboard/_fieldtexts/create')
-                ->assertDontSee(parent::visibilityText('Except on forms', 'create'))
-                ->assertDontSee(parent::visibilityText('Hide when creating', 'create'))
-                ->assertSee(parent::visibilityText('Hide when editing', 'create'))
-                ->assertSee(parent::visibilityText('Hide from index', 'create'))
-                ->assertSee(parent::visibilityText('Hide-From index', 'create'))
-                ->assertSee(parent::visibilityText('Hide from show', 'create'))
-                ->assertSee(parent::visibilityText('Only on forms', 'create'))
-                ->assertDontSee(parent::visibilityText('Only on index', 'create'))
-                ->assertDontSee(parent::visibilityText('Only on show', 'create'))
-                ->assertDontSee(parent::visibilityText('Visible-On index', 'create'));
+                ->visit('dashboard/' . $this->field)
+                //App\Providers\DuskServiceProvider
+                ->assertVisibility($this->field, $this->user);
         });
     }
 }
