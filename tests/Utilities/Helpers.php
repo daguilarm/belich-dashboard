b24f6de13e20a3811b0612a485e9256084f8a4f6
daguilarm <damian.aguilarm@gmail.com>
2019-10-28 20:20:39 +0100

    Created helper

--- tests/Utilities/Helpers.php
+++ tests/Utilities/Helpers.php
@@ -3,30 +3,5 @@
 namespace Tests\Utilities;
 
 trait Helpers {
-
-    /**
-     * Formating text for visibility test
-     *
-     * @param string $text
-     * @param string $action
-     * @return void
-     */
-    public function visibilityText(string $text, string $action) : string
-    {
-        if($action === 'index') {
-            return strtoupper($text);
-        }
-
-        if($action === 'show') {
-            return ucwords($text);
-        }
-
-        if($action === 'edit') {
-            return ucwords($text);
-        }
-
-        if($action === 'create') {
-            return ucwords($text);
-        }
-    }
+    //
 }
