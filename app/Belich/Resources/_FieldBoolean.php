559d2a9d82521e9d693e01a9ca47d87a2ff98439
daguilarm <damian.aguilarm@gmail.com>
2019-10-29 18:07:18 +0100

    Created boolean field test

---
+++ app/Belich/Resources/_FieldBoolean.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Fields\Types\ID;
+use Daguilarm\Belich\Fields\Types\Text;
+use Daguilarm\Belich\Resources;
+use Illuminate\Http\Request;
+
+class _FieldBoolean extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Boolean';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Boolean';
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request) {
+        return [
+            // Testing for visibility
+            Text::make('Hide from index', 'test_name')
+                ->hideFromIndex(),
+            Text::make('Hide from show', 'test_name')
+                ->hideFromShow(),
+            Text::make('Hide when creating', 'test_name')
+                ->hideWhenCreating(),
+            Text::make('Hide when editing', 'test_name')
+                ->hideWhenEditing(),
+            Text::make('Only on index', 'test_name')
+                ->onlyOnIndex(),
+            Text::make('Only on show', 'test_name')
+                ->onlyOnShow(),
+            Text::make('Only on forms', 'test_name')
+                ->onlyOnForms(),
+            Text::make('Except on forms', 'test_name')
+                ->exceptOnForms(),
+            Text::make('Visible-on index', 'test_name')
+                ->visibleOn('index'),
+            Text::make('Hide-From index', 'test_name')
+                ->hideFrom('index'),
+            //Testing for html render
+            Text::make('Testing attributes: html', 'test_html')
+                ->id('testing_asHtml')
+                ->asHtml(),
+            //Testing for attributes
+            Text::make('Testing attributes', 'test_name')
+                ->addClass('testing-class')
+                ->id('testing_id')
+                ->name('testing-name')
+                ->data('test', 'testing-data')
+                ->disabled()
+                ->readonly()
+                ->dusk('testing-dusk')
+                ->defaultValue('testing-value')
+                ->help('testing help'),
+            //Testing prefix
+            Text::make('Testing prefix', 'test_name')
+                ->id('testing_focus')
+                ->prefix('***')
+                ->suffix('***'),
+            //Testing model manipulation
+            Text::make('Testing model manipulation', 'test_name')
+                ->resolveUsing(function($model) {
+                    return 'resolved ' . $model->test_email;
+                }),
+            //Testing result manipulation
+            Text::make('Testing result manipulation', 'test_name')
+                ->displayUsing(function($value) {
+                    return strtoupper($value);
+                }),
+            //Testing authorization
+            Text::make('Testing authorization', 'test_name')
+                ->id('testing_can_see')
+                ->canSee(function($request) {
+                    return true;
+                }),
+            Text::make('Testing authorization', 'test_name')
+                ->id('testing_cannot_see')
+                ->canSee(function($request) {
+                    return false;
+                }),
+            //Testing autofocus
+            Text::make('Testing autofocus', 'test_autofocus')
+                ->id('test_autofocus')
+                ->autofocus(),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request) {
+        return;
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request) {
+        return;
+    }
+}
---
+++ tests/Browser/Fields/BooleanTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Browser\Fields;
+
+use App\Test;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+// dusk --filter BooleanTest
+class BooleanTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    protected $asHtml;
+    protected $field;
+    protected $test;
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->test = factory(Test::class)->create();
+        $this->field = '_fieldbooleans';
+        $this->asHtml = '<h1 class="text-red-500">Hello world</h1>';
+    }
+
+    /**
+     * Visibility test
+     *
+     * dusk --filter test_boolean_visibility
+     * @return void
+     */
+    public function test_boolean_visibility()
+    {
+        $this->browse(function (Browser $browser) {
+            // Index visibility
+            $browser
+                ->loginAs($this->user)
+                //App\Providers\DuskServiceProvider
+                ->assertVisibility($this->user, $this->field);
+        });
+    }
+
+    /**
+     * Attributes test
+     *
+     * dusk --filter test_boolean_attributes
+     * @return void
+     */
+    public function test_boolean_attributes()
+    {
+        $this->browse(function (Browser $browser) {
+            // Index visibility
+            $browser
+                ->loginAs($this->user)
+                //App\Providers\DuskServiceProvider
+                ->assertAttributes($this->user, $this->test, $this->field, $this->asHtml);
+        });
+    }
+
+    /**
+     * Autofocus test
+     *
+     * dusk --filter test_boolean_autofocus
+     * @return void
+     */
+    public function test_boolean_autofocus()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->loginAs($this->user)
+                ->visit('dashboard/' . $this->field . '/create')
+                ->assertFocused('#test_autofocus');
+        });
+    }
+
+    /**
+     * Authorization test
+     *
+     * dusk --filter test_boolean_authorization
+     * @return void
+     */
+    public function test_boolean_authorization()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->loginAs($this->user)
+                ->visit('dashboard/' . $this->field . '/create')
+                ->assertVisible('#testing_can_see')
+                ->assertMissing('#testing_cannot_see');
+        });
+    }
+}
