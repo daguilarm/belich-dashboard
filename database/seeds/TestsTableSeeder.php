4a3e0965da066e718ee90a7a20d8b9a111c0442a
daguilarm <damian.aguilarm@gmail.com>
2019-11-02 20:52:06 +0100

    Added a base field for testing

--- database/seeds/TestsTableSeeder.php
+++ database/seeds/TestsTableSeeder.php
@@ -13,6 +13,12 @@ class TestsTableSeeder extends Seeder
     public function run()
     {
         //Create the tests
+        factory(App\Test::class)->create([
+            'id' => 1,
+            'test_name' => 'Damian',
+            'test_string' => 'Testing belich or something',
+            'test_language' => 'php',
+        ]);
         factory(App\Test::class, 100)->create();
     }
 }
