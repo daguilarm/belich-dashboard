b6f5d116c6abb147bf75ddc0af279196f654b562
daguilarm <damian.aguilarm@gmail.com>
2019-11-02 17:12:28 +0100

    Updated routes: ajax

--- app/Belich/Routes.php
+++ app/Belich/Routes.php
@@ -11,6 +11,10 @@ Route::get(Belich::path(), function() {
     return view('belich::pages.dashboard');
 });
 
-//Ajax route
-Route::get(Belich::path() . '/ajax/example', '\App\Http\Controllers\AjaxController')
-    ->name('ajax.example');
+//Ajax routes
+Route::get(Belich::path() . '/ajax/user', '\App\Http\Controllers\AjaxUserController')
+    ->name('ajax.user');
+Route::get(Belich::path() . '/ajax/test/name', '\App\Http\Controllers\AjaxTestNameController')
+    ->name('ajax.test.name');
+Route::get(Belich::path() . '/ajax/test/string', '\App\Http\Controllers\AjaxTestStringController')
+    ->name('ajax.test.string');
