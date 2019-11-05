c9d0b7f28250746a2ea4471ffc746865781c8f39
daguilarm <damian.aguilarm@gmail.com>
2019-11-05 15:35:49 +0100

    Refactoring

--- app/Belich/Resources/Test.php
+++ app/Belich/Resources/Test.php
@@ -4,10 +4,10 @@ namespace App\Belich\Resources;
 
 use Daguilarm\Belich\Core\Resources;
 use Daguilarm\Belich\Fields\Types\ID;
-use Daguilarm\Belich\Fields\Types\Coordenates;
+use Daguilarm\Belich\Fields\Types\Text;
 use Illuminate\Http\Request;
 
-class _FieldCoordenate extends Resources {
+class Test extends Resources {
 
     /** @var string [Model path] */
     public static $model = '\App\Test';
@@ -22,10 +22,10 @@ class _FieldCoordenate extends Resources {
     public static $icon = 'vial';
 
     /** @var string */
-    public static $label = 'Field Coordenates';
+    public static $label = 'Test';
 
     /** @var string */
-    public static $pluralLabel = 'Fields Coordenates';
+    public static $pluralLabel = 'Tests';
 
     /**
      * Build the query for the given resource.
@@ -46,8 +46,24 @@ class _FieldCoordenate extends Resources {
     {
         return [
             //Testing for attributes
-            Coordenates::make('Testing attributes', 'test_coordenate')
-                ->addClass('hellow'),
+            Text::make('Testing attributes', 'test_name')
+                ->addClass('testing-class1', 'testing-class2')
+                ->id('testing_id')
+                ->name('testing-name')
+                ->data('test', 'testing-data')
+                ->dusk('testing-dusk')
+                ->defaultValue('testing-value')
+                ->help('testing help'),
+            Text::make('Testing disabled', 'test_string')
+                ->addClass('testing-class')
+                ->id('testing_id')
+                ->name('testing-name')
+                ->disabled(),
+            Text::make('Testing readonly', 'test_lastname')
+                ->addClass('testing-class')
+                ->id('testing_id')
+                ->name('testing-name')
+                ->readonly(),
         ];
     }
 
