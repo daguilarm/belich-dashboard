2e746e8ceb489d929fd29d18b0496de4081c7ae5
daguilarm <damian.aguilarm@gmail.com>
2019-11-07 15:09:15 +0100

    Empty helpers

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -3,15 +3,5 @@
 namespace Tests;
 
 trait Helpers {
-    /**
-     * Set field to be evaluated
-     *
-     * @param string $field
-     *
-     * @return string
-     */
-    protected static function setField(string $field): string
-    {
-        return '_field' . $field;
-    }
+    //Nothing
 }
