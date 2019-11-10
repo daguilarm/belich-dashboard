c14b7ac4e02fbfa858e23e7f82240cef359bf5dc
daguilarm <damian.aguilarm@gmail.com>
2019-11-10 19:09:51 +0100

    Create new tests

---
+++ app/Belich/Resources/_FieldImage.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\Image;
+use Daguilarm\Belich\Fields\Types\ID;
+use Illuminate\Http\Request;
+
+class _FieldImage extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Image';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Image';
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            Id::make('ID', 'id')
+                ->sortable(),
+            Image::make('File url', 'test_file')
+                ->addClass('block h-10 rounded-full shadow-md')
+                ->asHtml(),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
