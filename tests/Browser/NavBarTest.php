e3ccbade0691e7314612d1320ab2b834e35cfa8d
daguilarm <damian.aguilarm@gmail.com>
2019-11-10 12:23:50 +0100

    Fixed error with parsing url

--- tests/Browser/NavBarTest.php
+++ tests/Browser/NavBarTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser;
 
 use App\User;
+use Daguilarm\Belich\Facades\Helper;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -13,11 +14,13 @@ class NavBarTest extends DuskTestCase
     use DatabaseMigrations;
 
     protected $user;
+    protected $avatar;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->user = factory(User::class, 'admin')->create();
+        $this->avatar = htmlspecialchars(Helper::gravatar($this->user->email));
     }
 
     /**
@@ -36,12 +39,12 @@ class NavBarTest extends DuskTestCase
                 ->visit('/dashboard')
                 ->assertPathIs('/dashboard')
                 ->click('@logout')
+                ->assertSourceHas('<img src="' . $this->avatar . '" class="block h-10 rounded-full shadow-md" alt="My gravatar">')
                 ->click('@logout-profile')
                 ->assertPathIs('/dashboard/profiles/' . $this->user->id)
                 ->assertSee($this->user->id)
                 ->assertSee($this->user->name)
                 ->assertSee($this->user->email)
-                ->assertSourceHas($this->user->profile->profile_avatar)
                 ->click('@button-action-edit')
                 ->assertPathIs('/dashboard/profiles/' . $this->user->id . '/edit');
             //Test logout
