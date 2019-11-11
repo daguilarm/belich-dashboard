a055dc2676c75a3ee15eb77a0cefeaa250e70a79
daguilarm <damian.aguilarm@gmail.com>
2019-11-11 21:45:22 +0100

    Removed $dates

--- app/Test.php
+++ app/Test.php
@@ -3,19 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Test extends Model
 {
-    use SoftDeletes;
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
-
     /**
      * The attributes that are mass assignable.
      *
@@ -23,6 +13,7 @@ class Test extends Model
      */
     protected $fillable = [
         'id',
+        'test_password',
         'test_string',
         'test_language',
         'test_name',
