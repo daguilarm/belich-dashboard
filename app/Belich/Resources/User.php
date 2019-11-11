cea4f048137c1fcd3c86e32e1680da636a3a56fc
daguilarm <damian.aguilarm@gmail.com>
2019-11-11 21:46:23 +0100

    Removed unnecessary fields

--- app/Belich/Resources/User.php
+++ app/Belich/Resources/User.php
@@ -86,8 +86,7 @@ class User extends Resources {
                 ->withRelationship('profile'),
             Password::make('Password', 'password')
                 ->creationRules('required', 'required_with:password_confirmation', 'confirmed', 'min:6')
-                ->updateRules('nullable', 'required_with:password_confirmation', 'same:password_confirmation', 'min:6')
-                ->onlyOnForms(),
+                ->updateRules('nullable', 'required_with:password_confirmation', 'same:password_confirmation', 'min:6'),
             PasswordConfirmation::make('Password confirmation'),
         ];
     }
