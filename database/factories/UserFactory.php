42d206e671e371a277dd01ab3037d62809dbc6ba
daguilarm <damian.aguilarm@gmail.com>
2019-11-11 21:48:07 +0100

    Updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Str;
 
 $factory->defineAs(App\User::class, 'admin',  function ($faker) {
     return [
-        'name'              => 'admin',
-        'email'             => 'admin@email.com',
+        'name' => 'admin',
+        'email' => 'admin@email.com',
         'email_verified_at' => now(),
-        'password'          => bcrypt('admin'),
-        'remember_token'    => Str::random(10)
+        'password' => bcrypt('admin'),
+        'remember_token' => Str::random(10)
     ];
 });
 
@@ -32,12 +32,12 @@ $factory->afterCreatingState(App\User::class, 'admin', function ($user, $faker) 
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name'              => $faker->name,
-        'email'             => $faker->unique()->safeEmail,
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password'          => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token'    => Str::random(10),
-        'created_at'        => $faker->dateTimeBetween($startDate = '-1 year', $endDate = 'now'),
+        'password'  => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => Str::random(10),
+        'created_at' => $faker->dateTimeBetween($startDate = '-1 year', $endDate = 'now'),
     ];
 });
 
