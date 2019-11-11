d1f7c42f0904c3c6f3df187059bcd63117c377a8
daguilarm <damian.aguilarm@gmail.com>
2019-11-11 21:48:20 +0100

    Updated fields

--- tests/Browser/Fields/Custom/HeadersTest.php
+++ tests/Browser/Fields/Custom/HeadersTest.php
@@ -70,9 +70,9 @@ class HeadersTest extends DuskTestCase
             $browser
                 ->loginAs($this->user)
                 ->visit('dashboard/' . $this->field .'/create')
-                    ->assertSourceHas('<div class="w-full items-center py-5 px-6 font-bold text-gray-600 bg-gray-200"> Header 1 </div>')
-                    ->assertSourceHas('<h1 class="bg-gray-200 text-gray-700 p-5"><i class="fas fa-envelope-open-text icon"></i> Header 2</h1>')
-                    ->assertSourceHas('<div class="w-full items-center py-5 px-6 font-bold text-white bg-green-500"> Header 3 </div>');
+                    ->assertSourceHas('<div class="w-full items-center py-5 px-6 font-bold text-gray-600 bg-gray-300"> Header 1 </div>')
+                    ->assertSourceHas('<h1 class="bg-gray-800 text-white p-5"><i class="fas fa-envelope-open-text icon"></i> Header 2</h1>')
+                    ->assertSourceHas('<div class="w-full items-center py-5 px-6 font-bold text-green-500 bg-green-200"> Header 3 </div>');
         });
     }
 }
