d9e9ff0934873fa5333398ef3c306ce579aa6596
daguilarm <damian.aguilarm@gmail.com>
2019-11-12 11:53:07 +0100

    Testing selects

---
+++ app/Belich/Resources/_FieldSelectAction.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\Select;
+use Illuminate\Http\Request;
+
+class _FieldSelectAction extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Select: action';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Select: actions';
+
+    /**
+     * Generate constructor for the resource
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //Getting data from storage to populate the field
+        $this->selectNames = \App\User::pluck('name', 'id')->toArray();
+    }
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            //Testing options
+            Select::make('Testing options', 'test_string')
+                ->id('testing_options')
+                ->options([
+                    1 => 'Admin',
+                    2 => 'Manager',
+                    3 => 'User'
+                ]),
+            //Testing default value
+            Select::make('Testing default value', 'test_string')
+                ->id('testing_default')
+                ->options([
+                    1 => 'Admin',
+                    2 => 'Manager',
+                    3 => 'User'
+                ])
+                ->defaultValue(2),
+            //Testing from DB
+            Select::make('Testing DB', 'test_string')
+                ->id('testing_db')
+                ->options($this->selectNames),
+            //Testing display using labels
+            Select::make('Role', 'id')
+                ->id('testing_labels')
+                ->options([
+                    1 => 'Admin',
+                    2 => 'Manager',
+                    3 => 'User'
+                ])
+                ->displayUsingLabels()
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
---
+++ tests/Browser/Fields/Custom/SelectTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Browser\Fields\Custom;
+
+use App\Test;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Fields\Setup;
+use Tests\DuskTestCase;
+
+// dusk --filter SelectTest
+class SelectTest extends DuskTestCase
+{
+    use DatabaseMigrations, Setup;
+
+    protected $field;
+    protected $test;
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->test = factory(Test::class, 3)->create();
+        $this->field = $this->setField('selectactions');
+    }
+
+    /**
+     * Test select display using
+     *
+     * dusk --filter test_select_display
+     */
+    public function test_select_display()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->loginAs($this->user)
+                ->visit('dashboard/' . $this->field)
+                ->assertSourceHas('<td class="py-4 px-6 border-b border-solid border-gray-200 no-softdeleted"> Admin </td>')
+                ->assertSourceHas('<td class="py-4 px-6 border-b border-solid border-gray-200 no-softdeleted"> Manager </td>')
+                ->assertSourceHas('<td class="py-4 px-6 border-b border-solid border-gray-200 no-softdeleted"> User </td>')
+                ->assertSourceMissing('<td class="py-4 px-6 border-b border-solid border-gray-200 no-softdeleted"> 1 </td>')
+                ->assertSourceMissing('<td class="py-4 px-6 border-b border-solid border-gray-200 no-softdeleted"> 2 </td>')
+                ->assertSourceMissing('<td class="py-4 px-6 border-b border-solid border-gray-200 no-softdeleted"> 3 </td>');
+        });
+    }
+
+    /**
+     * Test select option values
+     *
+     * dusk --filter test_select_values
+     */
+    public function test_select_values()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->loginAs($this->user)
+                ->visit('dashboard/' . $this->field . '/create')
+                // Testing DB values
+                ->assertSelectHasOptions('#testing_db', User::pluck('id')->toArray())
+                // Testing array values
+                ->assertSelectHasOptions('#testing_options', [1, 2, 3])
+                ->assertSelectMissingOptions('#testing_options', [4, 5, 6])
+                // Testing default value
+                ->assertSelected('#testing_default', 2)
+                ->assertNotSelected('#testing_default', 1);
+        });
+    }
+}
