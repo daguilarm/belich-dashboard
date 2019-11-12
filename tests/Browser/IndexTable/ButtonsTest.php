40b84ed0f9efcf4a1cbd18f4b4866a383568d780
daguilarm <damian.aguilarm@gmail.com>
2019-11-12 23:12:20 +0100

    Testing index table buttons

--- tests/Browser/IndexTable/ButtonsTest.php
+++ tests/Browser/IndexTable/ButtonsTest.php
@@ -26,7 +26,7 @@ class ButtonsTest extends DuskTestCase
 
         $this->user = factory(User::class)->create();
         $this->field = $this->setField('texts');
-        factory(Test::class, 100)->create();
+        factory(Test::class, 15)->create();
     }
 
     /**
@@ -50,20 +50,20 @@ class ButtonsTest extends DuskTestCase
     /**
      * Table results per page button
      *
-     * dusk --filter test_table_perPage_buttons
+     * dusk --filter test_table_perPage_button
      * @return void
      */
-    public function test_table_perPage_buttons()
+    public function test_table_perPage_button()
     {
         $this->browse(function (Browser $browser) {
             //Per page results
             $browser
                 ->loginAs($this->user)
                 ->visit('/dashboard/' . $this->field)
-                ->assertPresent('@button-options-options')
-                ->assertMissing('.btn-dropdown-content')
+                ->assertVisible('@button-options-options')
+                ->assertMissing('#belich-form-options')
                 ->mouseover('@button-options-options')
-                ->assertPresent('.btn-dropdown-content')
+                ->assertVisible('#belich-form-options')
                 ->select('perPage', 10)
                 ->press('@table-options-submit')
                 ->assertPathIs('/dashboard/' . $this->field);
@@ -71,6 +71,57 @@ class ButtonsTest extends DuskTestCase
             // Assert there is only 10 result
             $tableRows = count($browser->driver->findElements(WebDriverBy::cssSelector('#belich-index-table > tbody > tr')));
             $this->assertEquals($tableRows, 10);
+        });
+    }
+
+    /**
+     * Table download button
+     *
+     * dusk --filter test_table_download_button
+     * @return void
+     */
+    public function test_table_download_button()
+    {
+        $this->browse(function (Browser $browser) {
+            //Download file
+            $browser
+                ->loginAs($this->user)
+                ->visit('/dashboard/' . $this->field)
+                ->assertMissing('@button-options-exports')
+                ->assertMissing('#belich-form-exports')
+                ->click('@index-table-select-all')
+                ->assertVisible('@button-options-exports')
+                ->mouseover('@button-options-exports')
+                ->assertVisible('#belich-form-exports')
+                ->assertSelectHasOptions('select[name=format]', ['xls', 'xlsx', 'csv'])
+                ->assertSelectHasOptions('select[name=quantity]', ['all', 'selected'])
+                ->assertVisible('@table-export-submit');
+        });
+    }
+
+    /**
+     * Table mass delete button
+     *
+     * dusk --filter test_table_mass_delete_button
+     * @return void
+     */
+    public function test_table_mass_delete_button()
+    {
+        $this->browse(function (Browser $browser) {
+            //Download file
+            $browser
+                ->loginAs($this->user)
+                ->visit('/dashboard/' . $this->field)
+                ->assertMissing('@button-options-delete')
+                ->assertMissing('@modal-mass-delete')
+                ->click('@index-table-select-all')
+                ->assertVisible('@button-options-delete')
+                ->click('@button-options-delete')
+                ->waitFor('#modal-mass-delete')
+                ->press('@modal-mass-delete-confirm-button');
+
+            // Assert all the items are deleted
+            $this->assertEquals(Test::all()->count(), 0);
         });
     }
 }
