970e11e286d1533586a8e016e1d980b6dca707c4
daguilarm <damian.aguilarm@gmail.com>
2019-11-14 15:03:31 +0100

    Testing new atributes: placeholder and pattern

---
+++ app/Belich/Resources/_FieldAttribute.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\Email;
+use Daguilarm\Belich\Fields\Types\Select;
+use Daguilarm\Belich\Fields\Types\Text;
+use Illuminate\Http\Request;
+
+class _FieldAttribute extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Attributes';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Attributes';
+
+    /** @var string */
+    public static $tableTextAlign = 'center';
+
+    /** @var array */
+    public static  $search = ['test_name'];
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            //Testing attributes in text
+            Text::make('Testing text', 'test_string')
+                ->dusk('testing-text')
+                ->placeholder('testing placeholder in text')
+                ->pattern('[a-z]{1,15}'),
+            //Testing attributes in text
+            Email::make('Testing email', 'test_email')
+                ->dusk('testing-email')
+                ->placeholder('testing placeholder in email')
+                ->pattern('[a-z]{1,15}'),
+            //Testing attributes in select
+            Select::make('Testing select', 'test_string')
+                ->dusk('testing-select')
+                ->options([])
+                ->placeholder('testing placeholder in select')
+                ->pattern('[a-z]{1,15}'),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
---
+++ tests/Browser/Fields/Custom/AttributesTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Browser\Fields\Custom;
+
+use App\Test;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Fields\Setup;
+use Tests\DuskTestCase;
+
+// dusk --filter AttributesTest
+class AttributesTest extends DuskTestCase
+{
+    use DatabaseMigrations, Setup;
+
+    protected $field;
+    protected $test;
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->test = factory(Test::class, 30)->create();
+        $this->field = $this->setField('attributes');
+    }
+
+    /**
+     * Testing email multiple
+     *
+     * dusk --filter test_email_multiple
+     * @return void
+     */
+    public function test_email_multiple()
+    {
+        $this->browse(function (Browser $browser) {
+            // Testing forms
+            $browser
+                ->loginAs($this->user)
+                ->visit('dashboard/' . $this->field . '/create')
+                ->assertSourceHas('<input class="mr-3" type="text" value="" dusk="testing-text" id="test_string" name="test_string" placeholder="testing placeholder in text" pattern="[a-z]{1,15}">')
+                ->assertSourceHas('<input class="mr-3" type="email" value="" dusk="testing-email" id="test_email" name="test_email" placeholder="testing placeholder in email" pattern="[a-z]{1,15}">')
+                ->assertSourceHas('<select class="block px-4 py-2 pr-8" dusk="testing-select" id="test_string" name="test_string" placeholder="testing placeholder in select" pattern="[a-z]{1,15}"> <option value=""></option> </select>');
+        });
+    }
+}
