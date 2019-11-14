c7cfef8c61210260de96f0a773dbb91dc5bbf67c
daguilarm <damian.aguilarm@gmail.com>
2019-11-14 19:17:01 +0100

    Tested: field range

---
+++ app/Belich/Resources/_FieldRange.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\Range;
+use Illuminate\Http\Request;
+
+class _FieldRange extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Range';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Range';
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            //Testing range
+            Range::make('Testing range', 'test_name')
+                ->id('test_range')
+                ->dusk('dusk-test_range')
+                ->min(1)
+                ->max(100)
+                ->step(10),
+            //Testing range with options
+            Range::make('Testing range options', 'test_name')
+                ->id('test_range_options')
+                ->dusk('dusk-test_range_options')
+                ->step(10)
+                ->options(['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
---
+++ tests/Browser/Fields/Custom/RangeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Browser\Fields\Custom;
+
+use App\Test;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Fields\Setup;
+use Tests\DuskTestCase;
+
+// dusk --filter RangeTest
+class RangeTest extends DuskTestCase
+{
+    use DatabaseMigrations, Setup;
+
+    protected $field;
+    protected $test;
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->test = factory(Test::class, 30)->create();
+        $this->field = $this->setField('ranges');
+    }
+
+    /**
+     * Testing range
+     *
+     * dusk --filter test_range_field
+     * @return void
+     */
+    public function test_range_field()
+    {
+        $this->browse(function (Browser $browser) {
+            // Testing forms
+            $browser
+                ->loginAs($this->user)
+                ->visit('dashboard/' . $this->field . '/create')
+                ->assertSourceHas('<input class="mr-3" type="range" value="" dusk="dusk-test_range" id="test_range" name="test_name" max="100" min="1" step="10" list="datalist_test_range">')
+                ->assertSourceHas('<input class="mr-3" type="range" value="" dusk="dusk-test_range_options" id="test_range_options" name="test_name" step="10" list="datalist_test_range_options"> <datalist id="datalist_test_range_options"> <option>0</option> <option>10</option> <option>20</option> <option>30</option> <option>40</option> <option>50</option> <option>60</option> <option>70</option> <option>80</option> <option>90</option> <option>100</option> </datalist>');
+        });
+    }
+}
