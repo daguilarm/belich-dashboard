0dcf1c95c2da3051a8e5bedd835b0df1e59fa63a
daguilarm <damian.aguilarm@gmail.com>
2019-11-14 22:16:14 +0100

    New fields

---
+++ app/Belich/Resources/_FieldTime.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\Time;
+use Illuminate\Http\Request;
+
+class _FieldTime extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Time';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Time';
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            //Testing time
+            Time::make('Testing time', 'test_name')
+                ->id('test_time')
+                ->dusk('dusk-test_time')
+                ->min('09:00')
+                ->max('22:00')
+                ->step(1),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
---
+++ app/Belich/Resources/_FieldUrl.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\Url;
+use Illuminate\Http\Request;
+
+class _FieldUrl extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Test';
+
+    /** @var array */
+    public static $relationships = ['user'];
+
+    /** @var string */
+    public static $group = 'Testing';
+
+    /** @var string */
+    public static $icon = 'vial';
+
+    /** @var string */
+    public static $label = 'Field Url';
+
+    /** @var string */
+    public static $pluralLabel = 'Fields Url';
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            //Testing url
+            Url::make('Testing url', 'test_url')
+                ->id('test_url')
+                ->dusk('dusk-test_url'),
+            //Testing url with options
+            Url::make('Testing url options', 'test_url')
+                ->id('test_url_options')
+                ->dusk('dusk-test_url_options')
+                ->defaultValue('http://www.laravel.com'),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
