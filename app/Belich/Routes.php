cc0a98fdf2d40b01dd4d69416edc63f3852660b6
daguilarm <damian.aguilarm@gmail.com>
2019-11-15 10:27:10 +0100

    Empty file

--- app/Belich/Routes.php
+++ app/Belich/Routes.php
@@ -5,16 +5,3 @@
 | Define your coustom routes
 |--------------------------------------------------------------------------
 */
-
-//Dashboard route
-Route::get(Belich::path(), function() {
-    return view('belich::pages.dashboard');
-});
-
-//Ajax routes
-Route::get(Belich::path() . '/ajax/user', '\App\Http\Controllers\AjaxUserController')
-    ->name('ajax.user');
-Route::get(Belich::path() . '/ajax/test/name', '\App\Http\Controllers\AjaxTestNameController')
-    ->name('ajax.test.name');
-Route::get(Belich::path() . '/ajax/test/string', '\App\Http\Controllers\AjaxTestStringController')
-    ->name('ajax.test.string');
