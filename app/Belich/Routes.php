e659d5e4d43698d3cbe8e15a33f7acf654175022
daguilarm <damian.aguilarm@gmail.com>
2019-11-15 13:30:04 +0100

    Added ajax routes for testing

--- app/Belich/Routes.php
+++ app/Belich/Routes.php
@@ -8,3 +8,11 @@
 
 // Dashboard / Home route
 Route::get(Belich::path(), '\App\Belich\Dashboard');
+
+// Testing routes
+Route::get(Belich::path() . '/ajax/user', '\App\Http\Controllers\AjaxUserController')
+    ->name('ajax.user');
+Route::get(Belich::path() . '/ajax/test/name', '\App\Http\Controllers\AjaxTestNameController')
+    ->name('ajax.test.name');
+Route::get(Belich::path() . '/ajax/test/string', '\App\Http\Controllers\AjaxTestStringController')
+    ->name('ajax.test.string');
---
+++ app/Http/Controllers/AjaxUserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AjaxUserController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function __invoke(Request $request)
+    {
+        $results = \App\User::Query()
+            ->select('id as value', 'name as label')
+            ->when($request->search, function($query) use ($request) {
+                return $query->where('name', 'like', '%' . $request->search . '%');
+            })
+            ->take(10)
+            ->get();
+
+        return response()->json($results);
+    }
+}
