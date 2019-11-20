620dd74857239d06ab6a42668b1663f24e244ece
daguilarm <damian.aguilarm@gmail.com>
2019-11-20 08:25:52 +0100

    Testing policies

---
+++ app/Belich/Resources/Policy.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Belich\Resources;
+
+use Daguilarm\Belich\Core\Resources;
+use Daguilarm\Belich\Fields\Types\ID;
+use Daguilarm\Belich\Fields\Types\Image;
+use Daguilarm\Belich\Fields\Types\Select;
+use Daguilarm\Belich\Fields\Types\Text;
+use Illuminate\Http\Request;
+
+class Policy extends Resources {
+
+    /** @var string [Model path] */
+    public static $model = '\App\Profile';
+
+    /** @var string */
+    public static $group = 'Policies';
+
+    /** @var string */
+    public static $icon = 'cogs';
+
+    /** @var string */
+    public static $label = 'Policy';
+
+    /** @var string */
+    public static $pluralLabel = 'Policies';
+
+    /**
+     * Build the query for the given resource.
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function indexQuery() {
+        return $this->model();
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            ID::make('Id'),
+            Text::make('User', 'name')
+                ->withRelationship('user'),
+            Text::make('Email', 'email')
+                ->withRelationship('user'),
+            Text::make('Address', 'profile_address')
+                ->rules('required'),
+            Image::make('Avatar', 'profile_avatar')
+                ->alt('Testing alt'),
+        ];
+    }
+
+    /**
+     * Set the custom metric cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function metrics(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Set the custom cards
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Illuminate\Support\Collection
+     */
+    public static function cards(Request $request): array
+    {
+        return [];
+    }
+}
---
+++ tests/Browser/PolicyTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Browser\IndexTable;
+
+use App\Test;
+use App\User;
+use Daguilarm\Belich\Facades\Helper;
+use Facebook\WebDriver\WebDriverBy;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Fields\Setup;
+use Tests\DuskTestCase;
+
+// dusk --filter PolicyTest
+class PolicyTest extends DuskTestCase
+{
+    use DatabaseMigrations, Setup;
+
+    protected $card;
+    protected $field;
+    protected $test;
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create(['id' => 1]); factory(User::class, 20)->create();
+        $this->test = factory(Test::class, 15)->create();
+        $this->field = 'policies';
+    }
+
+    /**
+     * Testing policies visibility
+     *
+     * dusk --filter test_policies_visibility
+     * @return void
+     */
+    public function test_policies_visibility()
+    {
+        $this->browse(function (Browser $browser) {
+            // Dashboard
+            $browser
+                ->loginAs($this->user->find(1))
+                ->visit('/dashboard/' . $this->field)
+                ->assertSourceHas('https://belich-dashboard.test/dashboard/policies/1')
+                ->assertSourceHas('https://belich-dashboard.test/dashboard/policies/1/edit')
+                ->assertSourceMissing('https://belich-dashboard.test/dashboard/policies/2')
+                ->assertSourceMissing('https://belich-dashboard.test/dashboard/policies/2/edit')
+                ->assertSourceMissing('https://belich-dashboard.test/dashboard/policies/3')
+                ->assertSourceMissing('https://belich-dashboard.test/dashboard/policies/3/edit');
+        });
+    }
+
+    /**
+     * Testing policies access
+     *
+     * dusk --filter test_policies_access
+     * @return void
+     */
+    public function test_policies_access()
+    {
+        $this->browse(function (Browser $browser) {
+            // Dashboard
+            $browser
+                ->loginAs($this->user->find(1))
+                ->visit('/dashboard/' . $this->field . '/1')
+                ->assertDontSee('403')
+                ->visit('/dashboard/' . $this->field . '/2')
+                ->assertSee('403')
+                ->visit('/dashboard/' . $this->field . '/1/edit')
+                ->assertDontSee('403')
+                ->visit('/dashboard/' . $this->field . '/2/edit')
+                ->assertSee('403')
+                ->visit('/dashboard/' . $this->field . '/create')
+                ->assertSee('403');
+        });
+    }
+}
