a8f074680d42b9d9f25526925b889a480c0bf6d3
daguilarm <damian.aguilarm@gmail.com>
2019-11-30 09:19:08 +0100

    Testing asColor()

--- tests/Browser/Fields/Custom/ColorTest.php
+++ tests/Browser/Fields/Custom/ColorTest.php
@@ -38,12 +38,20 @@ class ColorTest extends DuskTestCase
     public function test_color_field()
     {
         $this->browse(function (Browser $browser) {
-            // Testing forms
             $browser
                 ->loginAs($this->user)
+                // Testing index
+                ->visit('dashboard/' . $this->field)
+                ->assertSourceHas('<div class="w-12 h-2 rounded" style="background-color:#e66465">&nbsp;</div>')
+                // Testing show
+                ->visit('dashboard/' . $this->field . '/1')
+                ->assertSourceHas('<div class="w-12 h-2 rounded" style="background-color:#e66465">&nbsp;</div>')
+                // Testing forms
                 ->visit('dashboard/' . $this->field . '/create')
                 ->assertSourceHas('<input class="mr-3" type="color" dusk="dusk-test_color" id="test_color" name="test_color">')
-                ->assertSourceHas('<input class="mr-3" type="color" value="#e66465" dusk="dusk-test_color_multiple" id="test_color_multiple" name="test_name">');
+                ->assertSourceHas('<input class="mr-3" type="color" value="#e66465" dusk="dusk-test_color_multiple" id="test_color_multiple" name="test_name">')
+                ->assertSourceMissing('<div class="w-12 h-2 rounded" style="background-color:#e66465">&nbsp;</div>');
+
         });
     }
 }
